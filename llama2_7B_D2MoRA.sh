fc1ef20477d72be2cf45e15a775edc79e19175ef
Jianhui <48922150+zuojianh@users.noreply.github.com>
2025-08-06 21:23:40 +0800

    Update llama2_7B_D2MoRA.sh
    Signed-off-by: Jianhui <48922150+zuojianh@users.noreply.github.com>

--- llama2_7B_D2MoRA.sh
+++ llama2_7B_D2MoRA.sh
@@ -11,7 +11,7 @@ export weight_path='checkpoints/D2MoRA'
 
 export device_id=0,1
 
-CUDA_VISIBLE_DEVICES=$device_id nohup torchrun --nproc_per_node 2 --master_port 34517 finetune.py \
+CUDA_VISIBLE_DEVICES=$device_id torchrun --nproc_per_node 2 --master_port 34517 finetune.py \
    --base_model $model_path \
    --data_path 'LLM-Adapters/commonsense_170k.json' \
    --output_dir $weight_path \
@@ -21,4 +21,4 @@ CUDA_VISIBLE_DEVICES=$device_id nohup torchrun --nproc_per_node 2 --master_port 
    --use_orth_loss \
    --adapter_name d2mora --expert_down 4 --expert_up 3 \
    --target_modules '["q_proj", "k_proj", "v_proj"]' \
-   --lora_r $1 --lora_alpha $2 --use_gradient_checkpointing &
+   --lora_r $1 --lora_alpha $2 --use_gradient_checkpointing
